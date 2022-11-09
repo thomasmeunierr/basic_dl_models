78d13445468100652b9541614d929024a5e6c955
Thomas Meunier <73098849+thomasmeunierr@users.noreply.github.com>
2022-11-09 14:55:07 +0100

    Update ResNet.py

--- ResNet.py
+++ ResNet.py
@@ -15,30 +15,33 @@ class ResNetUnit(nn.Module):
 
         super(ResNetUnit, self).__init__(**kwargs)
         self.stride = stride
-        self.seq = nn.Sequential(
-            nn.Conv2d(input_channels, nb_channels, kernel_size, 1, "same"),
-            nn.BatchNorm2d(),
-            nn.ReLU(True),
-            nn.Conv2d(nb_channels, nb_channels, kernel_size, 1, "same"),
-            nn.BatchNorm2d(),
-        )
 
-        self.skip_seq = nn.Sequential(
-            nn.Conv2d(input_channels, nb_channels, kernel_size, stride, "same"),
-            nn.BatchNorm2d(),
+        
+        self.seq = nn.Sequential(
+            nn.Conv2d(input_channels, nb_channels, kernel_size, stride, 1,bias = False),
+            nn.BatchNorm2d(nb_channels),
+            nn.ReLU(True),
+            nn.Conv2d(nb_channels, nb_channels, kernel_size, 1, 1,bias = False),
+            nn.BatchNorm2d(nb_channels),
         )
+        self.skip_layers = nn.Sequential()
+
+        if stride > 1 :
+
+            self.skip_layers = nn.Sequential(nn.Conv2d(input_channels,nb_channels,1,stride,bias = False),
+                                            nn.BatchNorm2d(nb_channels))
 
     def forward(self, x):
 
         out = self.seq(x)
 
-        if self.stride == 1:
+        skip = x
 
-            return x + out
+        if self.stride > 1 :
 
-        else:
-
-            return out + self.skip_seq(x)
+            skip = self.skip_layers(skip)
+        
+        return nn.functional.relu(out + skip)
 
 
 class ResNet34(nn.Module):
@@ -47,38 +50,21 @@ class ResNet34(nn.Module):
         super(ResNet34, self).__init__()
 
         self.first_layers = nn.Sequential(
-            nn.Conv2d(3, 64, 7, 2, "same"), nn.MaxPool2d(3, 2, "same")
+            nn.Conv2d(3, 64, 7, 2, 3,bias = False), nn.MaxPool2d(3, 2, 1)
         )
 
         layers = []
-        for _ in range(3):
-
-            layers.append(ResNetUnit(64, 64))
-
-        layers.append(ResNetUnit(64, 128, 3, 2))
-
-        for _ in range(4):
-
-            layers.append(ResNetUnit(128, 128))
-
-        layers.append(ResNetUnit(128, 256, 3, 2))
-
-        for _ in range(6):
-
-            layers.append(ResNetUnit(256, 256))
-
-        layers.append(ResNetUnit(256, 512, 3, 2))
-
-        for _ in range(3):
-
-            layers.append(ResNetUnit(512, 512))
-
-        layers.append(ResNetUnit(512, 1024, 3, 2))
+        prev = 64
+        
+        for filters in [64]*3 + [128]*4 + [256]*6 + [512]*3:
+            strides = 1 if filters == prev else 2
+            layers.append(ResNetUnit(prev,filters,3,strides))
+            prev = filters
 
         self.deep_layers = nn.Sequential(*layers)
 
         self.final_layers = nn.Sequential(
-            nn.AvgPool2d(), nn.Flatten(), nn.Linear(1024 * 7 * 7, 10), nn.Softmax()
+            nn.AdaptiveAvgPool2d(output_size=(1, 1)), nn.Flatten(), nn.Linear(512, 1000), nn.Softmax()
         )
 
     def forward(self, x):
